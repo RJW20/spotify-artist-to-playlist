7f9f5046ce2d503748e12d6f1e30a665e8a67f5e
RJW20 <robertjwatts84@outlook.com>
2024-05-06 14:44:58 +0100

    Add retrieval of user information

--- spotify_artist_to_playlist/authentication.py
+++ spotify_artist_to_playlist/authentication.py
@@ -9,3 +9,12 @@ def get_token(sp_dc_cookie) -> dict:
     r = requests.get(url, headers=this_req_headers)
 
     return {'authorization': f"Bearer {r.json()['accessToken']}"}
+
+def get_user_id(headers: dict) -> str:
+    """Return the user_id of the user with the authorization header in headers."""
+
+    url = "https://api.spotify.com/v1/me"
+    r = requests.get(url, headers=headers)
+    data = r.json()
+
+    return data['id']
