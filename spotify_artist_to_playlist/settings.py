e1e9720f8526960f1a1ca15b45ac24b06007c83f
RJW20 <robertjwatts84@outlook.com>
2024-05-07 11:06:27 +0100

    Add settings template

---
+++ spotify_artist_to_playlist/settings.py
@@ -0,0 +1,9 @@
+
+
+settings = {
+
+    'sp_dc_cookie': '',
+    'artist_id': '',
+    'unwanted_versions': ['Live', 'Acoustic', 'Instrumental', 'Acapella'],   # Will try to remove any versions that match
+
+}
